0f1bcc76c74ffef8d9fccccb717f03e2e0550cf4
SUB03 <nortdip@gmail.com>
2025-12-23 01:58:27 +0500

    fixed prop names

--- frontend/src/app/components/MenuItem.tsx
+++ frontend/src/app/components/MenuItem.tsx
@@ -2,15 +2,20 @@ export interface MenuItemProps {
   name: string;
   description: string;
   price: string;
-  image: string;
+  image_url: string;
 }
 
-export function MenuItem({ name, description, price, image }: MenuItemProps) {
+export function MenuItem({
+  name,
+  description,
+  price,
+  image_url,
+}: MenuItemProps) {
   return (
     <div className="group cursor-pointer">
       <div className="aspect-[4/3] overflow-hidden rounded-lg mb-4 bg-neutral-800 border border-amber-500/20">
         <img
-          src={image}
+          src={image_url}
           alt={name}
           className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
         />
